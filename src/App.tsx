090722dcac2a57a10c5b9ba8f0300144ce577d5c
Federico Herrera Modernizacion <fedeeherrera2001@gmail.com>
2024-05-23 12:00:32 -0300

    Finalizacion del proyecto TODO

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,43 @@
-import { useState } from 'react'
-import Todos from './components/Todos'
-import { type TodoId, type TodoCompleted } from './types'
-
-const mockTodos = [
-  { id: '1', title: 'Estudiar', completed: false },
-  { id: '2', title: 'Pagina Web Zapas', completed: true },
-  { id: '3', title: 'Trabajar', completed: false },
-]
+import { Copyright } from './components/Copyright'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { Todos } from './components/Todos'
+import { useTodos } from './hooks/useTodos'
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState(mockTodos)
-
-  const handleRemove = ({ id }: TodoId): void => {
-    const newTodos = todos.filter((todo) => todo.id !== id)
-    setTodos(newTodos)
-  }
-
-  const handleCompleted = ({
-    id,
-    completed,
-  }: {
-    id: TodoId
-    completed: TodoCompleted
-  }): void => {
-    const newTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          completed,
-        }
-      }
-      return todo
-    })
-    setTodos(newTodos)
-  }
+  const {
+    activeCount,
+    completedCount,
+    filterSelected,
+    handleClearCompleted,
+    handleCompleted,
+    handleFilterChange,
+    handleRemove,
+    handleSave,
+    handleUpdateTitle,
+    todos: filteredTodos
+  } = useTodos()
 
   return (
-    <div className="todoapp">
-      <h1>Todo APP</h1>
-      <Todos todos={todos} onRemoveTodo={handleRemove} onToggleCompleteTodo= {handleCompleted} />
-    </div>
+    <>
+      <div className='todoapp'>
+        <Header saveTodo={handleSave} />
+        <Todos
+          removeTodo={handleRemove}
+          setCompleted={handleCompleted}
+          setTitle={handleUpdateTitle}
+          todos={filteredTodos}
+        />
+        <Footer
+          handleFilterChange={handleFilterChange}
+          completedCount={completedCount}
+          activeCount={activeCount}
+          filterSelected={filterSelected}
+          onClearCompleted={handleClearCompleted}
+        />
+      </div>
+      <Copyright />
+    </>
   )
 }
 
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,8 @@
+import './Copyright.css'
+
+export const Copyright: React.FC = () => (
+  <footer className='copyright'>
+    <h3>Proyect creado gracias a ⚛️@midudev⚛️</h3>
+    <h5>Creando un TODO con TypeScript</h5>
+  </footer>
+)
